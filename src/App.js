c263ec8a07cebc2eee72f1cb35c79a7ac0e09979
skipGit <komanitskiygit@gmail.com>
2022-01-03 13:44:06 +0200

    AddNewItemForm-component-creation
    1. Added component for creating new "ToDo's" based on submiting text input form.
    2. Additional styling required.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { theme } from "./assets/theme";
 import { ThemeProvider } from "@mui/material";
 import ToDoList from "./templates/to-do-list/ToDoList";
+import AddNewItemForm from "./templates/to-do-list/add-new-item-form/AddNewItemForm";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <AddNewItemForm />
       <ToDoList />
     </ThemeProvider>
   );
